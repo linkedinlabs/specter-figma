2db65d78617f3ccc9423458fb36182ef1bf2b52b
Grant Blakeman <hello@grantblakeman.com>
2019-08-12 17:37:25 -0700

    stub in menu commands

--- src/gui.ts
+++ src/gui.ts
@@ -1,23 +1,23 @@
 import './views/webview.css';
 
-document.getElementById('annotate').onclick = () => {
-  const textbox = (<HTMLInputElement> document.getElementById('count'));
-  const count = parseInt(textbox.value, 10);
-  parent.postMessage({
-    pluginMessage: {
-      type: 'create-rectangles',
-      count,
-    },
-  }, '*');
-};
+const actions = (<HTMLInputElement> document.getElementById('actions')); 
 
-document.getElementById('measure').onclick = () => {
-  const textbox = (<HTMLInputElement> document.getElementById('count'));
-  const count = parseInt(textbox.value, 10);
-  parent.postMessage({
-    pluginMessage: {
-      type: 'lawls',
-      count,
-    },
-  }, '*');
-};
+if (actions) {
+  const onClick = (e: MouseEvent) => {
+    const button = e.target.closest('button');
+    
+     if (button) {
+       // find action by element id
+       const action = button.id;
+
+       // bubble message to main
+       parent.postMessage({
+         pluginMessage: {
+           type: action,
+         },
+       }, '*');
+     }
+  }
+
+  actions.addEventListener('click', onClick);
+}
