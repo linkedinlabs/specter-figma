257575de389f4fe725771947a85553546a0ad23f
Grant Blakeman <hello@grantblakeman.com>
2019-08-14 18:24:55 -0700

    add constants

---
+++ src/constants.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * @description The public-facing name for the plugin. This should match the
+ * `name` stated in manifset.json.
+ *
+ * @kind constant
+ * @name PLUGIN_NAME
+ * @type {string}
+ */
+const PLUGIN_NAME = 'Specter';
+
+const COLORS = {
+  component: '#9966ff',
+  custom: '#ff3399',
+  dimension: '#99cc00',
+  spacing: '#00cc99',
+  style: '#ff6655',
+};
+
+export {
+  COLORS,
+  PLUGIN_NAME,
+};
+/* eslint-enable import/prefer-default-export */
