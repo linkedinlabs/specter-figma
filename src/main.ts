905e50f8a07f5d90c69ae1e4837dfd07f11e87c2
Grant Blakeman <hello@grantblakeman.com>
2019-08-19 17:52:32 -0600

    set up dispatch to ferry GUI/menu commands

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,130 @@
-// This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+// ++++++++++++++++++++++++++ Specter for Figma +++++++++++++++++++++++++++
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "webview.html" which has a
-// full browser enviroment (see documentation).
+// invoked commands -------------------------------------------------
 
-const getRandomInt = (min: number, max: number): number => {
-  const setMin = Math.ceil(min);
-  const setMax = Math.floor(max);
-  const num = Math.floor(Math.random() * (setMax - setMin + 1)) + setMin;
-  return num;
+/** WIP
+ * @description Identifies and annotates a selected layer in a Sketch file.
+ *
+ * @kind function
+ * @name annotateLayer
+ * @param {Object} context The current context (event) received from Sketch.
+ * @returns {null} Shows a Toast in the UI if nothing is selected.
+ */
+const annotateLayer = (closeGui: boolean): void => {
+  console.log('action: annotateLayer');
+
+  if (closeGui) {
+    figma.closePlugin();
+  }
+  return null;
 };
 
-// watch menu commands
-if (figma.command) {
-  if (figma.command === 'tools') {
-    // This shows the HTML page in "webview.html".
-    figma.showUI(__html__, { width: 140, height: 180 }); // eslint-disable-line no-undef    
-  } else {
-    console.log(`Do the thing: ${figma.command}`);
+/** WIP
+ * @description Annotates a selected layer in a Sketch file with user input.
+ *
+ * @kind function
+ * @name annotateLayerCustom
+ * @param {Object} context The current context (event) received from Sketch.
+ * @returns {null} Shows a Toast in the UI if nothing is selected or
+ * if multiple layers are selected.
+ */
+const annotateLayerCustom = (closeGui: boolean): void => {
+  console.log('action: annotateLayerCustom');
+
+  if (closeGui) {
     figma.closePlugin();
+  }
+  return null;
+};
+
+/** WIP
+ * @description Annotates a selection of layers in a Sketch file with the
+ * spacing number (“IS-X”) based on the gap between the two layers.
+ *
+ * @kind function
+ * @name annotateMeasurement
+ * @param {Object} context The current context (event) received from Sketch.
+ * @returns {null} Shows a Toast in the UI if nothing is selected or
+ * if more than two layers are selected.
+ */
+const annotateMeasurement = (closeGui: boolean): void => {
+  console.log('action: annotateMeasurement');
+
+  if (closeGui) {
+    figma.closePlugin();
+  }
+  return null;
+};
+
+/** WIP
+ * @description Draws a semi-transparent “Bounding Box” around any selected elements.
+ *
+ * @kind function
+ * @name drawBoundingBox
+ * @param {Object} context The current context (event) received from Sketch.
+ * @returns {null} Shows a Toast in the UI if nothing is selected.
+ */
+const drawBoundingBox = (closeGui: boolean = true): void => {
+  console.log('action: drawBoundingBox');
+
+  if (closeGui) {
+    figma.closePlugin();
+  }
+  return null;
+};
+
+// watch for commands -------------------------------------------------
+/** WIP
+ * @description Identifies and annotates a selected layer in a Sketch file.
+ *
+ * @kind function
+ * @name dispatch
+ * @param {string} actionType A string representing the action received from the GUI.
+ * @returns {null}
+ */
+const dispatch = (action: {
+  type: string,
+  visual: boolean,
+}): void => {
+  const closeGui: boolean = !action.visual;
+  switch (action.type) {
+    case 'annotate':
+      annotateLayer(closeGui);
+      break;
+    case 'annotate-custom':
+      annotateLayerCustom(closeGui);
+      break;
+    case 'bounding':
+      drawBoundingBox(closeGui);
+      break;
+     case 'measure':
+       annotateMeasurement(closeGui);
+       break;
+     default:
+       // show UI – command: tools
+       figma.showUI(__html__, { width: 140, height: 180 }); // eslint-disable-line no-undef
   }
 }
 
-// watch GUI action clicks
+// watch menu commands -------------------------------------------------
+if (figma.command) {
+  dispatch({
+    type: figma.command,
+    visual: false,
+  });
+}
+
+// watch GUI action clicks -------------------------------------------------
 figma.ui.onmessage = (msg): void => {
-  console.log(`Do the thing: ${msg.type}`);
-
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (
-      let i = 0; i < msg.count; i += 1
-    ) {
-      const rect = figma.createRectangle();
-      rect.x = i * 20;
-      rect.y = i * 20;
-
-      const r = (getRandomInt(0, 255) / 255);
-      const g = (getRandomInt(0, 255) / 255);
-      const b = (getRandomInt(0, 255) / 255);
-
-      rect.fills = [{
-        type: 'SOLID',
-        color: { r, g, b },
-      }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  if (msg.type) {
+    dispatch({
+      type: msg.type,
+      visual: true,
+    });
   }
 
-  if (msg.type === 'lawls') {
-    console.log('hullo');
-    figma.ui.resize(300, 400);
-  }
+  // if (msg.type === 'lawls') {
+  //   console.log('hullo');
+  //   figma.ui.resize(300, 400);
+  // }
 };
