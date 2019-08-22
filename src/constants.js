efe082751dfe6f267915968bd73164135b4264f7
Grant Blakeman <hello@grantblakeman.com>
2019-08-21 18:39:52 -0600

    closing the plugin - better handling

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 
 /**
+ * @description A unique string to identify the plugin within Figma.
+ * Changing this will break data retrieval in any `sharedPluginData` and potentially elsewhere.
+ * [More info]{@link https://www.figma.com/plugin-docs/api/properties/nodes-setsharedplugindata/}
+ *
+ * @kind constant
+ * @name PLUGIN_IDENTIFIER
+ * @type {string}
+ */
+const PLUGIN_IDENTIFIER = 'com.linkedin.figma.specter-plugin';
+
+/**
  * @description The public-facing name for the plugin. This should match the
  * `name` stated in manifset.json.
  *
@@ -25,9 +36,13 @@ const GUI_SETTINGS = {
   },
 };
 
+const CLOSE_PLUGIN_MSG = '_CLOSE_PLUGIN_';
+
 export {
+  CLOSE_PLUGIN_MSG,
   COLORS,
   GUI_SETTINGS,
+  PLUGIN_IDENTIFIER,
   PLUGIN_NAME,
 };
 /* eslint-enable import/prefer-default-export */
