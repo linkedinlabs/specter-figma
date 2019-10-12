be7acf52586bf35ad35ffd6a23f7bb5ade4fb1fc
Grant Blakeman <hello@grantblakeman.com>
2019-10-11 18:58:10 -0700

    messenger - add typing

--- src/Messenger.ts
+++ src/Messenger.ts
@@ -77,7 +77,7 @@ export default class Messenger {
       // set up toast and log messages
       const toastMessage = result.messages.toast;
       const logMessage = result.messages.log;
-      const isError = (result.status === 'error');
+      const isError: boolean = (result.status === 'error');
 
       // log a message or error
       if (logMessage) {
