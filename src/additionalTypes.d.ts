6b134c65869d4cee902db4685619d9f00d56feef
Grant Blakeman <hello@grantblakeman.com>
2020-09-08 17:33:36 -0700

    typescript - remove redundent Figma typings + rename file

---
+++ src/additionalTypes.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  // Internal Declarations
+
+  // Vendor Declarations
+
+  // for attaching Svelte to window global
+  interface Window {
+    app: Function;
+  }
+
+  // Figma’s typings in npm package @figma/plugin-typings
+} // declare global
+
+export {}
