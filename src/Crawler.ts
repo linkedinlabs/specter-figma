2eb8620652730fde44290ecfbba8c885086c2a81
Grant Blakeman <hello@grantblakeman.com>
2020-09-10 16:07:51 -0700

    crawler - update “all” and add “allSorted”

--- src/Crawler.ts
+++ src/Crawler.ts
@@ -36,10 +36,13 @@ export default class Crawler {
 
   /**
    * @description Looks into the selection array for any groups and pulls out individual nodes,
-   * effectively flattening the selection.
+   * effectively flattening the selection. NOTE: Component and Instance types are included to
+   * allow Specter to annotate at the top-level. If they are excluded, components nested within
+   * components will also be annotated.
    *
    * @kind function
    * @name all
+   *
    * @returns {Object} All items (including children) individual in an updated array.
    */
   all() {
@@ -51,19 +54,32 @@ export default class Crawler {
       if (
         node.type !== CONTAINER_NODE_TYPES.group
         && node.type !== CONTAINER_NODE_TYPES.frame
+        // && node.type !== CONTAINER_NODE_TYPES.component
+        // && node.type !== CONTAINER_NODE_TYPES.instance
+        && node.visible
+        && !node.locked
       ) {
         // non-frame or -group nodes get added to the final selection
         flatSelection.push(node);
       } else {
+        // +++ frames and groups are added for their own styles to be evaluated
+        flatSelection.push(node);
+
         // +++ frames and groups are checked for child nodes
 
         // set initial holding array and add first level of children
-        let innerNodes = [];
-        node.children.forEach(child => innerNodes.push(child));
+        let innerLayers = [];
+        if (node.visible && !node.locked) {
+          node.children.forEach((child) => {
+            if (child.visible && !node.locked) {
+              innerLayers.push(child);
+            }
+          });
+        }
 
         /**
-         * @description Iterates through `innerNodes`, adding normal nodes to the `flatSelection`
-         * array, while adding any additional children nodes into the `innerNodes` array.
+         * @description Iterates through `innerLayers`, adding normal nodes to the `flatSelection`
+         * array, while adding any additional children nodes into the `innerLayers` array.
          *
          * @kind function
          * @name iterateKnownChildren
@@ -72,48 +88,88 @@ export default class Crawler {
          * @private
          */
         const iterateKnownChildren = (): void => {
-          // iterate through known child nodes in `innerNodes`,
+          // iterate through known child nodes in `innerLayers`,
           // adding more children to the array as they are found in descendent nodes
-          innerNodes.forEach((
-            innerNode: {
+          innerLayers.forEach((
+            innerLayer: {
               children: any,
               id: string,
               type: string,
               visible: boolean,
+              locked: boolean,
             },
           ) => {
             if (
-              innerNode.type !== CONTAINER_NODE_TYPES.group
-              && innerNode.type !== CONTAINER_NODE_TYPES.frame
-              && innerNode.visible
+              innerLayer.type !== CONTAINER_NODE_TYPES.group
+              && innerLayer.type !== CONTAINER_NODE_TYPES.frame
+              // && innerLayer.type !== CONTAINER_NODE_TYPES.component
+              // && innerLayer.type !== CONTAINER_NODE_TYPES.instance
+              && innerLayer.visible
+              && !innerLayer.locked
             ) {
               // non-frame or -group nodes get added to the final selection
-              flatSelection.push(innerNode);
-            } else {
-              innerNode.children.forEach(child => innerNodes.push(child));
+              flatSelection.push(innerLayer);
+            } else if (innerLayer.visible && !innerLayer.locked) {
+              // frames and groups are added for their own styles to be evaluated
+              flatSelection.push(innerLayer);
+
+              innerLayer.children.forEach((child) => {
+                if (child.visible && !child.locked) {
+                  innerLayers.push(child);
+                }
+              });
             }
 
             // update the overall list of child nodes, removing the node that was just examined.
             // this array should eventually be empty, breaking the `while` loop
-            const innerNodeIndex = innerNodes.findIndex(
-              foundInnerNode => (foundInnerNode.id === innerNode.id),
+            const innerLayerIndex = innerLayers.findIndex(
+              foundInnerLayer => (foundInnerLayer.id === innerLayer.id),
             );
-            innerNodes = [
-              ...innerNodes.slice(0, innerNodeIndex),
-              ...innerNodes.slice(innerNodeIndex + 1),
+            innerLayers = [
+              ...innerLayers.slice(0, innerLayerIndex),
+              ...innerLayers.slice(innerLayerIndex + 1),
             ];
           });
 
           return null;
         };
 
-        // loop through the `innerNodes` array as long as it is not empty
-        while (innerNodes.length > 0) {
+        // loop through the `innerLayers` array as long as it is not empty
+        while (innerLayers.length > 0) {
           iterateKnownChildren();
         }
       }
     });
+
     return flatSelection;
+  }
+
+  /**
+   * @description Looks into the selection array for any groups and pulls out individual nodes,
+   * effectively flattening the selection.
+   *
+   * @kind function
+   * @name allSorted
+   *
+   * @returns {Object} All items (including children) individual in an updated array.
+   */
+  allSorted() {
+    // start with flattened selection of all nodes
+    const nodes = this.all();
+
+    // sort by `y` position and then `x` if `y` values are equal
+    const sortByPosition = (nodeA, nodeB) => {
+      const aPos = { x: nodeA.absoluteTransform[0][2], y: nodeA.absoluteTransform[1][2] };
+      const bPos = { x: nodeB.absoluteTransform[0][2], y: nodeB.absoluteTransform[1][2] };
+
+      if (aPos.y === bPos.y) {
+        return aPos.x - bPos.x;
+      }
+      return aPos.y - bPos.y;
+    };
+
+    const sortedLayers = nodes.sort(sortByPosition);
+    return sortedLayers;
   }
 
   /**
