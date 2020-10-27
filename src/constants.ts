433e21f84d35d9a982bd8ccf2ffa8a216b71b960
Grant Blakeman <hello@grantblakeman.com>
2020-10-27 14:50:49 -0700

    constants - add bundle key for stapler data

--- src/constants.ts
+++ src/constants.ts
@@ -32,6 +32,7 @@ const PLUGIN_NAME = 'Specter';
  */
 const DATA_KEYS = {
   options: `${PLUGIN_IDENTIFIER}.options-001`,
+  bundle: `${PLUGIN_IDENTIFIER}.bundle-001`,
   keystopNodeData: `${PLUGIN_IDENTIFIER}.keystopNodeData-001`,
   keystopAnnotations: `${PLUGIN_IDENTIFIER}.keystopAnnotations-001`,
   keystopList: `${PLUGIN_IDENTIFIER}.keystopList-001`,
